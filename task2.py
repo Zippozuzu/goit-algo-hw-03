c5956f11cecb0f1f567a3437e0c0f94ba911b76d
firebasetaras@gmail.com <firebasetaras@gmail.com>
2024-07-01 20:43:42 +0200

    Add number range and quantity check

--- task2.py
+++ task2.py
@@ -7,8 +7,8 @@ def get_numbers_ticket(min, max, quantity):
     if type(min) != int or type(max) != int or type(quantity) != int: #check if args are int 
         return lottery_numbers
     
-    if min < 1 or max > 1000:
-        print ("The numbers should be in 1 - 1000 range")
+    if min < 1 or max > 1000 or max < quantity:
+        print ("The numbers should be in 1 - 1000 range OR quantity is not correct")
         return lottery_numbers
 
     lottery_numbers =  random.sample(range(min, max), quantity)
@@ -17,4 +17,4 @@ def get_numbers_ticket(min, max, quantity):
     return lottery_numbers
 
 
-#print (get_numbers_ticket("-11", 44, 4))                             
+print (get_numbers_ticket(3, 44, 45))                             
